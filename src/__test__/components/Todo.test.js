d02836686001dae9e38f041753688a81726007c9
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-12-14 19:05:22 +0000

    Move component tests to their own folder

---
+++ src/__test__/components/Todo.test.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { configure, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import Todo from '../../components/Todo'
+
+configure({ adapter: new Adapter() })
+
+describe('Todo', () => {
+
+  const newTodo = {
+    task: 'Register for the Apple Developer Program',
+    completed: false,
+    id: 1
+  }
+
+  const deleteMock = jest.fn()
+
+  const todo = shallow(<Todo todo={newTodo} delete={deleteMock} />)
+
+  describe('when toggling the completion status', () => {
+    it('sets the text style to line-through', () => {
+      todo.find('.btn-toggle-item').simulate('click')
+
+      const actualStyle = todo.find('.task-text').prop('style')
+      const expectedStyle = { textDecoration: 'line-through', textTransform: 'uppercase' }
+      expect(actualStyle).toEqual(expectedStyle)
+    })
+
+    it('sets the text style back to none', () => {
+      todo.find('.btn-toggle-item').simulate('click')
+
+      const actualStyle = todo.find('.task-text').prop('style')
+      const expectedStyle = { textDecoration: 'none', textTransform: 'uppercase' }
+      expect(actualStyle).toEqual(expectedStyle)
+    })
+  })
+
+  describe('When clicking on the delete button of a todo', () => {
+    it('calls the delete prop', () => {
+      todo.find('.btn-delete-item').simulate('click')
+
+      expect(deleteMock).toHaveBeenCalled()
+    })
+  })
+})
---
+++ src/__test__/components/TodoList.test.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { configure, mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import TodoList from '../../components/TodoList'
+
+configure({ adapter: new Adapter() })
+
+describe('TodoList', () => {
+
+  describe('when adding a new item', () => {
+
+    const todoList = mount(<TodoList />)
+
+    const newItem = 'Get my car serviced'
+    todoList.find('input').first().simulate('change', { target : { value: newItem } })
+    todoList.find('form').simulate('submit', { preventDefault: () => {} })
+
+    it('it appears on the todo list', () => {
+      expect(todoList.children().find('li').exists()).toBe(true)
+    })
+  })
+
+  describe('when a todo is deleted', () => {
+
+    const todoList = mount(<TodoList />)
+    
+    const newItem = 'Sell my Xbox One'
+    todoList.find('input').first().simulate('change', { target : { value: newItem } })
+    todoList.find('form').simulate('submit', { preventDefault: () => {} })
+
+    todoList.setState({
+      task: '',
+      items: [],
+      nextId: 2
+    })
+    
+    it('no longer appears on the list', () => {
+      expect(todoList.find('.todo-item').exists()).toBe(false)
+    })
+  })
+})

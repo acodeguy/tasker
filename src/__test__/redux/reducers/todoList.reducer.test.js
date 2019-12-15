99799611c81fe456efd8f534ff41f6f1d3fc4c26
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-12-15 18:32:00 +0000

    TodoList reducer adds a new todo to the list

--- src/__test__/redux/reducers/todoList.reducer.test.js
+++ src/__test__/redux/reducers/todoList.reducer.test.js
@@ -13,4 +13,31 @@ describe('TodoList reducer', () => {
 
     expect(todoListReducer(initialState, action)).toEqual(expectedState)
   })
+
+  it('adds a todo item', () => {
+    const task = 'Learn something new'
+
+    const action = { 
+      type: constants.ADD_TODO,
+      payload: task
+     }
+
+    const initialState = {
+      nextID: 0,
+      items: []
+    }
+
+    const expectedState = {
+      nextID: 1,
+      items: [{
+        id: 0,
+        completed: false,
+        task,
+      }]
+    }
+
+    const actualState = todoListReducer(initialState, action)
+
+    expect(actualState).toEqual(expectedState)
+  })
 })
--- src/redux/reducers/todoList.reducer.js
+++ src/redux/reducers/todoList.reducer.js
@@ -7,6 +7,18 @@ const todoList = (state = {}, action) => {
         nextID: state.nextID +1
       }
 
+    case constants.ADD_TODO:
+      return {
+        nextID: state.nextID + 1,
+        items: [
+          {
+            id: state.nextID,
+            completed: false,
+            task: action.payload
+          }
+        ]
+      }
+
       default: 
       return state
   }

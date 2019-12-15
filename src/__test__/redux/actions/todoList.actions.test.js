7dc7c81b45507c11680b1d9fb1f7fb5e5e66d4d7
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-12-15 18:15:38 +0000

    Action creator returns an action to increment to todolist ID counter

--- src/__test__/redux/actions/todoList.actions.test.js
+++ src/__test__/redux/actions/todoList.actions.test.js
@@ -2,6 +2,16 @@ import * as constants from '../../../redux/constants'
 import * as actions from '../../../redux/actions/todoList.actions'
 
 describe('TodoList action creator', () => {
+  it('returns an action that increments the ID counter', () => {
+    const counter = 0
+    const expectedAction = {
+      type: constants.INCREMENT_TODO_ID_COUNTER,
+      payload: counter + 1
+    }
+
+    expect(actions.incrementTodoIDCounter(counter)).toEqual(expectedAction)
+  })
+
   it('returns an action that creates a new todo item', () => {
       const task = 'Learn Italian cooking'
 

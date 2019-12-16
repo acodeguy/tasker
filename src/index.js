04590fe7d44507ed1cd08172c52cf4a3e0f60b31
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-12-16 17:45:05 +0000

    Add React/React-Redux to project, setup store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './containers/App'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from '../src/redux/reducers/todoList.reducer'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-render(<App />, document.getElementById('root'))
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+)
